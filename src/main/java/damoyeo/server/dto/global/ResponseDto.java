8b46a87650b393495437076f16084e19254062d0
Changsu Park <gd12091@gmail.com>
2024-12-26 22:38:27 +0900

    feat: BoilerPlate

---
+++ src/main/java/damoyeo/server/dto/global/ResponseDto.java
@@ -0,0 +1,54 @@
+package damoyeo.server.dto.global;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import damoyeo.server.exception.CommonException;
+import damoyeo.server.exception.ErrorCode;
+import io.micrometer.common.lang.Nullable;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+
+public record ResponseDto<T>(
+        @JsonIgnore HttpStatus httpStatus,
+        @NotNull boolean success,
+        @Nullable T data,
+        @Nullable ExceptionDto error
+) {
+    public static <T> ResponseDto<T> ok(T data) {
+        return new ResponseDto<>(
+                HttpStatus.OK,
+                true,
+                data,
+                null
+        );
+    }
+
+    public static <T> ResponseDto<T> created(T data) {
+        return new ResponseDto<>(
+                HttpStatus.CREATED,
+                true,
+                data,
+                null
+        );
+    }
+
+    public static ResponseDto<Object> fail(@NotNull CommonException e) {
+        return new ResponseDto<>(
+                e.getErrorCode().getHttpStatus(),
+                false,
+                null,
+                ExceptionDto.of(e.getErrorCode())
+        );
+    }
+
+    public static ResponseDto<Object> fail(final MissingServletRequestParameterException e) {
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST, false, null,
+                ExceptionDto.of(ErrorCode.MISSING_REQUEST_PARAMETER));
+    }
+
+    public static ResponseDto<Object> fail(final MethodArgumentTypeMismatchException e) {
+        return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR, false, null,
+                ExceptionDto.of(ErrorCode.INVALID_PARAMETER_FORMAT));
+    }
+}
