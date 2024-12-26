2f9a418b0d38885f65766c50c0df275c424990ce
Changsu Park <gd12091@gmail.com>
2024-12-26 22:38:10 +0900

    feat: BoilerPlate

---
+++ src/main/java/damoyeo/server/exception/CommonException.java
@@ -0,0 +1,16 @@
+package damoyeo.server.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CommonException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    @Override
+    public String getMessage() {
+        return this.errorCode.getMessage();
+    }
+
+}
