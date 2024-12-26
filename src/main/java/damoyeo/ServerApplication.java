7b63783cf9ac3ade29fccbeb72148e5bc38a8eca
Changsu Park <gd12091@gmail.com>
2024-12-26 22:19:37 +0900

    Initial commit

--- src/main/java/damoyeo/ServerApplication.java
+++ src/main/java/damoyeo/ServerApplication.java
@@ -1,4 +1,4 @@
-package damoyeo.server;
+package damoyeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/damoyeo/ServerApplicationTests.java
+++ src/test/java/damoyeo/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package damoyeo.server;
+package damoyeo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
